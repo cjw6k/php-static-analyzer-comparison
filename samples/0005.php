fcf10adf9db379299043b4ef57d105de484a71a0
cjw6k <cj@w6k.ca>
2025-02-05 19:50:25 -0400

    emoji in source

---
+++ samples/0005.php
@@ -0,0 +1,16 @@
+<?php
+
+#️⃣this is my class
+class 💩💩💩💩
+{
+    #️⃣this is my💩 method
+	function 💩💩💩($😎, $🐯)
+	{
+		return $😎 + $🐯;
+	}
+
+}
+$🐔 = 3;
+$😥 = $🐔 + 2;
+$💩 = new 💩💩💩💩;
+echo $💩 -> 💩💩💩($🐔, $😥);
